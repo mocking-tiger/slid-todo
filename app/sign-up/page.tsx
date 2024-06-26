b4268f24fd595b4bd4cb40df1e5186cbfd62ae92
Park JunYong <dyden119@gmail.com>
2024-06-26 22:13:53 +0900

    design: 회원가입 페이지 퍼블리싱

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -3,16 +3,19 @@ import Input from "@/components/Input";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default function SignUp() {
   return (
     <main className="h-screen py-12 sm:py-16 xl:py-[120px] select-none">
       <div className="w-[343px] sm:w-[640px] mx-auto">
         <div className="w-fit mx-auto mb-[40px] py-[20.3px] pl-[12.7px] pr-[13.2px]">
-          <Image src="/logo.svg" width={244} height={48} alt="로고" />
+          <Link href="/">
+            <Image src="/logo.svg" width={244} height={48} alt="로고" />
+          </Link>
         </div>
         <div className="mb-[48px] flex flex-col gap-[24px]">
+          <Input span="이름" placeholder="이름을 입력해주세요" />
           <Input
-            span="아이디"
+            span="이메일"
             placeholder="이메일을 입력해주세요"
             type="email"
           />
@@ -21,12 +24,17 @@ export default function Home() {
             placeholder="비밀번호를 입력해주세요"
             isPassword
           />
+          <Input
+            span="비밀번호 확인"
+            placeholder="비밀번호를 다시 한 번 입력해주세요"
+            isPassword
+          />
         </div>
         <Button>로그인하기</Button>
         <p className="w-fit mx-auto text-[1.4rem]">
-          슬리드 투 두가 처음이신가요?{" "}
-          <Link href="/sign-up" className="text-[#3182F6] text-[1.4rem]">
-            회원가입
+          이미 회원이신가요?{" "}
+          <Link href="/" className="text-[#3182F6] text-[1.4rem]">
+            로그인
           </Link>
         </p>
       </div>
