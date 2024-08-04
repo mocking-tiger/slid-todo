2f529bd2c6175483fffca895810b12505cef5a02
Park JunYong <dyden119@gmail.com>
2024-08-04 23:25:09 +0900

    design: 사이드바 프로필 퍼블리싱

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useUserStore } from "@/zustand/userStore";
+import Image from "next/image";
+import Link from "next/link";
 import { useState } from "react";
 
 export default function SideBar() {
@@ -10,21 +12,52 @@ export default function SideBar() {
   return (
     <aside>
       <div
-        className={`w-[280px] h-screen border-r float-left ${
+        className={`w-[280px] h-screen px-[16px] py-[12px] border-r float-left ${
           isHide && "hidden"
         }`}
       >
-        <p>사이드바</p>
-        <h2>{user.email}</h2>
-        <h2>{user.name}</h2>
-        <button className="border" onClick={() => setIsHide((prev) => !prev)}>
-          {"<<"}
-        </button>
+        <div className="mb-[13px] flex justify-between items-center">
+          <Link href="/">
+            <Image src="/logo.svg" width={106} height={35} alt="logo-sidebar" />
+          </Link>
+          <button
+            className="p-[8px] border-2 rounded-[8px]"
+            onClick={() => setIsHide((prev) => !prev)}
+          >
+            <Image
+              src="/sidebar-hide.svg"
+              width={8}
+              height={8}
+              alt="sidebar-button"
+            />
+          </button>
+        </div>
+        <div className="px-[8px] flex gap-[12px]">
+          <Image
+            src="/sidebar-profile.svg"
+            width={64}
+            height={64}
+            alt="profile-sidebar"
+          />
+          <div>
+            <h2 className="">{user.name}</h2>
+            <h2>{user.email}</h2>
+            <button className="text-[1.2rem] text-gray-400">로그아웃</button>
+          </div>
+        </div>
       </div>
       {isHide && (
-        <div className="w-[32px] h-screen border-r float-left">
-          <button className="border" onClick={() => setIsHide((prev) => !prev)}>
-            {">>"}
+        <div className="w-fit h-screen px-[14px] py-[16px] border-r float-left">
+          <button
+            className="p-[8px] border-2 rounded-[8px]"
+            onClick={() => setIsHide((prev) => !prev)}
+          >
+            <Image
+              src="/sidebar-hide-R.svg"
+              width={8}
+              height={8}
+              alt="sidebar-button"
+            />
           </button>
         </div>
       )}
