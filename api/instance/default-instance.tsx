bb8e8a5580bd73251f88657b5d5eef9d1d66a30c
Park JunYong <dyden119@gmail.com>
2024-08-13 19:39:53 +0900

    fix: 토큰 재발급 못받는 문제 수정

--- api/instance/default-instance.tsx
+++ api/instance/default-instance.tsx
@@ -26,23 +26,30 @@ instance.interceptors.response.use(
   async (error) => {
     if (error.response.status === 401) {
       const token = async () => {
-        const getNewToken = await instance.post(`${BASE_URL}auth/tokens`);
+        const refreshToken = Cookies.get("refreshToken");
+        const getNewToken = await axios.post(`${BASE_URL}auth/tokens`, null, {
+          headers: {
+            accept: "application/json",
+            Authorization: `Bearer ${refreshToken}`,
+          },
+        });
+        console.log("토큰 정보 재발급 성공");
         Cookies.set("accessToken", getNewToken.data.accessToken, {
           expires: 1,
         });
         Cookies.set("refreshToken", getNewToken.data.refreshToken, {
           expires: 1,
         });
-
-        error.config.headers = {
-          Authorization: `Bearer ${Cookies.get("accessToken")}`,
-        };
-        const response = await axios.request(error.config);
-        console.log("instance에서 토큰 재발급");
-        return response;
       };
       await token();
+      error.config.headers = {
+        Authorization: `Bearer ${Cookies.get("accessToken")}`,
+      };
+      const response = await axios.request(error.config);
+      console.log("instance에서 토큰 재발급");
+      return response;
     }
+    return Promise.reject(error);
   }
 );
 
