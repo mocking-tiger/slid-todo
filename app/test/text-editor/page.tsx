aa71b906b0f446f2b90c237ce9c81f99b5b45a5e
Park JunYong <dyden119@gmail.com>
2024-09-02 21:30:54 +0900

    docs: 테스트 폴더 정리

---
+++ app/test/text-editor/page.tsx
@@ -0,0 +1,5 @@
+// import TextEditor from "@/components/Editor";
+
+export default function Test2() {
+  return <div>{/* <TextEditor /> */}</div>;
+}
--- app/test2/page.tsx
+++
@@ -1,9 +0,0 @@
-import TextEditor from "@/components/Editor";
-
-export default function Test2() {
-  return (
-    <div>
-      <TextEditor />
-    </div>
-  );
-}
