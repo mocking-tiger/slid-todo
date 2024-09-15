10c603c4a7e8c601e5ebbbca68ad27a768b4f67a
Park JunYong <dyden119@gmail.com>
2024-09-15 21:57:23 +0900

    chore: 메타데이터 테스트2

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import ToastProvider from "@/components/ToastProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://slid-todo.com"),
   title: "Slid-todo",
   description:
     "유저가 다양한 콘텐츠(아티클, 강의 영상, 줌 미팅 일정, 강의 PDF 등)를 할일 목록으로 관리하고, 학습 진도나 프로젝트 진행 상황을 대시보드로 보여주며 각 할일에 대한 노트를 작성해 관리하는 서비스",
